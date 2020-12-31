01c53391722d1ecace5f9374754c0bc865272a80
index011 <76804167+index011@users.noreply.github.com>
2020-12-30 22:16:16 -0800

    Add files via upload

---
+++ assets/js/shizzo.js
@@ -0,0 +1,10 @@
+function modal(title, body, btn){
+  document.getElementById('page_dialog_title').innerText = title;
+  document.getElementById('page_dialog_body').innerText = body;
+  document.getElementById('page_dialog_btn').innerText = btn;
+  document.getElementById('page_dialog').style.visibility= 'visible';
+}
+
+function hideModal(){
+  document.getElementById('page_dialog').style.visibility= 'hidden';
+}
